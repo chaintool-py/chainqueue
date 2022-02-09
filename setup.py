84227a525ea3395ed79e18b72548242343ed3f5d
William Luke <williamluke4@gmail.com>
2022-02-09 12:56:02 +0300

    Release v0.6.0rc2

--- setup.py
+++ setup.py
@@ -12,23 +12,11 @@ while True:
     requirements.append(l.rstrip())
 f.close()
 
-test_requirements = []
-f = open('test_requirements.txt', 'r')
-while True:
-    l = f.readline()
-    if l == '':
-        break
-    test_requirements.append(l.rstrip())
-f.close()
+postgres_requirements = ['psycopg2==2.8.6'] + requirements
+sqlite_requirements = [] + requirements
 
-postgres_requirements = [
-    'psycopg2==2.8.6',
-        ] + requirements
-sqlite_requirements = [
-        ] + requirements
 setup(
         install_requires=requirements,
-        tests_require=test_requirements,
         extras_require={
             'postgres': postgres_requirements,
             'sqlite': sqlite_requirements,
